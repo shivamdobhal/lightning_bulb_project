022f223048bc48b3b71ef6002a41a08bdfbdfda5
Shiv@m dobh@l <73071952+shivamdobhal@users.noreply.github.com>
2023-06-05 12:04:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+ document.getElementById("offimg").src="of.png"
+
+ function clickon(){
+    document.getElementById("offimg").src="on.png"
+
+ }
+
+ function clickoff(){
+    document.getElementById("offimg").src="of.png"
+
+ }
